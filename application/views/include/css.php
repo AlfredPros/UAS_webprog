a62e5f57d2e1eb413f7ebde9bae691d07aa71475
AlfredPros <alfredkuhlman@yahoo.co.id>
2021-11-29 20:38:43 +0700

    bye aller

--- application/views/include/css.php
+++ application/views/include/css.php
@@ -2,16 +2,3 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- Data Tables-->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
-
-<style>
-    @font-face {
-        font-family: Aller;
-        src: url('<?= base_url('assets/Aller.ttf')?>') format('truetype');
-    }
-    @import url('<?= base_url('assets/Aller.ttf')?>');
-
-    body {
-        font-family: 'Aller';
-    }
-
-</style>
