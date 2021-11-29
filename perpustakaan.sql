f749d7e92e29029c5530f4c27cc5fd12b2c9a0d1
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-11-29 15:14:14 +0800

    Update perpustakaan.sql
    tambah table

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2021 at 06:35 PM
+-- Generation Time: Nov 29, 2021 at 08:13 AM
 -- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- PHP Version: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,18 +33,31 @@ CREATE TABLE `list_buku` (
   `tahun_terbit` int(4) NOT NULL,
   `penulis_buku` varchar(255) NOT NULL,
   `penerbit_buku` varchar(255) NOT NULL,
-  `link_poster` varchar(255) NOT NULL
+  `link_poster` varchar(255) NOT NULL,
+  `deskripsi_buku` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `list_buku`
 --
 
-INSERT INTO `list_buku` (`id_buku`, `judul_buku`, `tahun_terbit`, `penulis_buku`, `penerbit_buku`, `link_poster`) VALUES
-(2, 'Gyate Gyate', 2020, 'Zen Kyara', 'ZeroRespect', 'assets/poster/cb5231121c6a9ffef23894e92b35e1e2.png'),
-(5, 'Menaricc Roll', 2019, 'Alert', 'Maki Ligon', 'assets/poster/97a0c7ce4572c22f7e48ecefeab7d69a.png'),
-(6, 'Food and Musik', 2017, 'Elegant Sister', 'Sakuzyo', 'assets/poster/78f637e9c5e924f23d103931df8126f7.png'),
-(7, 'Alert - Blue Archive', 2021, 'KARUT', 'Maki Ligon', 'assets/poster/a893170fefb73096dfe2e1b806572fbd.png');
+INSERT INTO `list_buku` (`id_buku`, `judul_buku`, `tahun_terbit`, `penulis_buku`, `penerbit_buku`, `link_poster`, `deskripsi_buku`) VALUES
+(2, 'Gyate Gyate', 2020, 'Zen Kyara', 'ZeroRespect', 'assets/poster/cb5231121c6a9ffef23894e92b35e1e2.png', ''),
+(5, 'Menaricc Roll', 2019, 'Alert', 'Maki Ligon', 'assets/poster/97a0c7ce4572c22f7e48ecefeab7d69a.png', ''),
+(6, 'Food and Musik', 2017, 'Elegant Sister', 'Sakuzyo', 'assets/poster/78f637e9c5e924f23d103931df8126f7.png', ''),
+(7, 'Alert - Blue Archive', 2021, 'KARUT', 'Maki Ligon', 'assets/poster/a893170fefb73096dfe2e1b806572fbd.png', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `id_request` int(11) NOT NULL,
+  `id_user` bigint(20) DEFAULT NULL,
+  `id_buku` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -54,16 +67,19 @@ INSERT INTO `list_buku` (`id_buku`, `judul_buku`, `tahun_terbit`, `penulis_buku`
 
 CREATE TABLE `user` (
   `id_user` bigint(20) NOT NULL,
-  `password` varchar(1000) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `password`) VALUES
-(3, '20a4c7d7bd12a1437f86357edde840fcff61e5f81897ac92f56d01db9ccfeb24493beb0e66e2e4e8e25684f7d3c02002f85f2fa23ad4fbac5ad42fb9ba904d9e'),
-(4, 'cd1839476f44a1b93c5d442b671a99bbdd55dcd829c319a479460a5c58925f3cf7d4fa080c65b92de4375687b9616f78e28b431deb820bda2e3c76805fabe264');
+INSERT INTO `user` (`id_user`, `email`, `password`, `name`, `role`) VALUES
+(3, '', '20a4c7d7bd12a1437f86357edde840fcff61e5f81897ac92f56d01db9ccfeb24493beb0e66e2e4e8e25684f7d3c02002f85f2fa23ad4fbac5ad42fb9ba904d9e', '', ''),
+(4, '', 'cd1839476f44a1b93c5d442b671a99bbdd55dcd829c319a479460a5c58925f3cf7d4fa080c65b92de4375687b9616f78e28b431deb820bda2e3c76805fabe264', '', '');
 
 --
 -- Indexes for dumped tables
@@ -74,6 +90,14 @@ INSERT INTO `user` (`id_user`, `password`) VALUES
 --
 ALTER TABLE `list_buku`
   ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id_request`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_buku` (`id_buku`);
 
 --
 -- Indexes for table `user`
@@ -96,6 +120,17 @@ ALTER TABLE `list_buku`
 --
 ALTER TABLE `user`
   MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `request_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `list_buku` (`id_buku`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
