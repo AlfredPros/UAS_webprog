ba7952bd5e480e1d3b5526da8081f22dfde7e599
Adhitya Bagus Wicaksono <adhityabagus80@gmail.com>
2021-12-02 01:34:43 +0700

    Update error aja

--- application/views/pages/error.php
+++ application/views/pages/error.php
@@ -1,24 +1,46 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php
-        echo $js;
-        echo $css;
+    echo $js;
+    echo $css;
     ?>
-    <title>Page not found! OwO</title>
+    <title>Page not found!</title>
+    <link rel="icon" href="<?= base_url('assets/images/ThumbnailLogo.png') ?>">
+
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        .container-fluid {
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .home {
+            color: #C90000;
+            padding-top: auto;
+            padding-bottom: auto;
+        }
+    </style>
 </head>
+
 <body>
     <?php
-        echo $header;
+    echo $header;
     ?>
 
-    <div class="container" style="padding-top:25px">
+    <div class="container-fluid home">
         <div class="row" style="padding-bottom:25px">
-            <h1 class="text-center">Oof! Page doesn't exist.</h1>
+            <h1 class="text-center" style="font-size: 100px;">Oof! Page doesn't exist.</h1>
         </div>
-
-        <image src="<?= base_url("assets/images/91552656_1.jpg") ?>" draggable="false" class="mx-auto d-block img-fluid">
     </div>
 
 </body>
+
 </html>
